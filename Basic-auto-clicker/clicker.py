e6aeb5392147534a7c278a76765574f11d0a79bf
Filip Krzyzanowski <filip.krzyzanowski@protonmail.com>
2021-01-14 14:16:41 +0100

    First stable version of program

---
+++ Basic-auto-clicker/clicker.py
@@ -0,0 +1,36 @@
+import time
+from pynput.mouse import Button, Controller
+mouse = Controller()
+
+from pynput.keyboard import Controller
+keyboard = Controller()
+
+# Set mouse coordinates (X, Y). If you want check coordinates use checkCoordinates.py
+mouse.position = (900, 500)
+
+# The number of repetitions of your click list
+for clicks in range(2):
+    # Waiting for next click in seconds
+    time.sleep(1)
+
+    # Number of clicks and mouse button
+    mouse.click(Button.left, 1)
+
+    # Print one letter, number and two chars with delay
+    keyboard.press('A')
+    keyboard.release('A')
+
+    time.sleep(1)
+
+    keyboard.press('1')
+    keyboard.release('1')
+
+    time.sleep(1)
+
+    keyboard.press('@')
+    keyboard.release('@')
+
+    time.sleep(1)
+
+    keyboard.press('@')
+    keyboard.release('@')
---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+## Actual version: 1.0
+
+#### v1.0, release date: 14.01.2021
+- Created basic auto clicker
+- Created script to checking mouse coordinates
+- A new release of the program has been created 
+
+#### 13.01.2021
+- Created repository
+- Created README and CHANGELOG files
